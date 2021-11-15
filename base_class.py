dbb7ebf146d9b6232052f4712cb3c817156aaa0e
bben98 <49661965+bben98@users.noreply.github.com>
2021-11-15 17:54:24 -0500

    Add files via upload

---
+++ base_class.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from random import randint
+
+def __init__(self, dataset):  
+    self = self
+    
+
+def train(self, function = None):
+    if (function == None):
+        print('Function is not applied')
+
+
+def val(self, function = None):
+    if (function == None):
+        print('Function is not applied')
+
+def test(self, function = None):
+    if (function == None):
+        print('Function is not applied')
