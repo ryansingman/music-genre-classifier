1f593766df5382c4e6e12e8363f1d69dffabf1d9
bben98 <49661965+bben98@users.noreply.github.com>
2021-11-30 12:06:34 -0500

    Update knn.py

--- music_genre_classifier/models/knn.py
+++ music_genre_classifier/models/knn.py
@@ -8,6 +8,17 @@ from .base import ModelTrainable
 
 class KNN(ModelTrainable):
     """Implements trainable KNN class."""
+    
+    def __init__(self, train_ds: np.ndarray, test_ds: np.ndarray):
+        """Initializes neural network trainable.
+        Parameters
+        ----------
+        train_ds : np.ndarray
+            dataset to train model with
+        test_ds : np.ndarray
+            dataset to test model with
+        """
+        super().__init__(train_ds, test_ds)
 
     def _tune(self) -> Dict:
         """Performs hyperparameter tuning for KNN model and returns best hyperparams.
@@ -17,6 +28,7 @@ class KNN(ModelTrainable):
         Dict
             best hyperparameters found in search
         """
+        
         ...
 
     def _train(self, hyperparams: Dict) -> keras.Model:
