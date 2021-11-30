8519d1a4f21272b0344717848a1ba2a5df691147
Ryan Singman <ryanjs@vt.edu>
2021-11-30 16:30:12 -0500

    ugly bug fixes, working

--- music_genre_classifier/models/knn.py
+++ music_genre_classifier/models/knn.py
@@ -1,20 +1,21 @@
 from typing import Dict
 
-import numpy as np
 import keras
 import keras_tuner as kt
+import numpy as np
 import pandas as pd
-from sklearn.preprocessing import scale
+from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
+from sklearn.preprocessing import scale
+
 from .base import ModelTrainable
 from music_genre_classifier import dataset
 
 
 class KNN(ModelTrainable):
     """Implements trainable KNN class."""
-    
+
     def __init__(self, train_ds: np.ndarray, test_ds: np.ndarray):
         """Initializes neural network trainable.
         Parameters
@@ -34,11 +35,19 @@ class KNN(ModelTrainable):
         np.array
             Normalized array of features and labels
         """
+<<<<<<< HEAD
         labels = array[:,-1]
         scaled_ds = scale(array[:,:-1])
         scaled_ds = np.concatenate((scaled_ds, np.array(labels)[:,None]),axis=1)
         scaled_features, scaled_labels = dataset.split_features_and_labels(scaled_ds)
         return scaled_features, scaled_labels
+=======
+        labels = array[:, -1]
+        scaled_ds = scale(array[:, :-1])
+        scaled_ds = np.concatenate((scaled_ds, np.array(labels)[:, None]), axis=1)
+        scaled_train_features, scaled_train_labels = dataset.split_features_and_labels(scaled_ds)
+        return scaled_train_features, scaled_train_labels
+>>>>>>> c14113c... ugly bug fixes, working
 
     def _tune(self) -> Dict:
         """Performs hyperparameter tuning for KNN model and returns best hyperparams.
@@ -48,14 +57,19 @@ class KNN(ModelTrainable):
         Dict
             best hyperparameters found in search
         """
-        #Normalize train dataset to fit knn
+        # Normalize train dataset to fit knn
         scaled_train_features, scaled_train_labels = self.normalized_dataset(self._train_ds)
-        
+
         # perform hyperparameter search and get best hyperparameters
-        hyperparameters = { 'n_neighbors' : list(range(1,20)),
-                       'weights' : ['uniform','distance'],
-                       'metric' : ['euclidean','minkowski','manhattan']}
-        grid_search = GridSearchCV(KNeighborsClassifier(), hyperparameters, verbose = 1, cv=3, n_jobs = -1)
+        hyperparameters = {
+            'n_neighbors': list(range(1, 20)),
+            'weights': ['uniform', 'distance'],
+            'metric': ['euclidean', 'minkowski', 'manhattan'],
+        }
+        grid_search = GridSearchCV(
+            KNeighborsClassifier(
+            ), hyperparameters, verbose=1, cv=3, n_jobs=-1,
+        )
         fit = grid_search.fit(scaled_train_features, scaled_train_labels)
 
         return fit.best_params_
@@ -73,18 +87,19 @@ class KNN(ModelTrainable):
         sklearn.Model
             trained sklearn model
         """
-        #Normalize train dataset to fit knn
+        # Normalize train dataset to fit knn
         scaled_train_features, scaled_train_labels = self.normalized_dataset(self._train_ds)
-        
-        #Build knn model with hyperparameters
-        knn = KNeighborsClassifier(weights= hyperparams['weights'], 
-                                     n_neighbors= hyperparams['n_neighbors'],
-                                     metric= hyperparams['metric'])
-        
-        #Create trained model
-        trained_model = knn.fit(scaled_train_features,scaled_train_labels)
+
+        # Build knn model with hyperparameters
+        knn = KNeighborsClassifier(
+            weights=hyperparams['weights'],
+            n_neighbors=hyperparams['n_neighbors'],
+            metric=hyperparams['metric'],
+        )
+
+        # Create trained model
+        trained_model = knn.fit(scaled_train_features, scaled_train_labels)
         return trained_model
-        
 
     def test(self):
         """Test function to see accuracy of knn.
@@ -94,10 +109,10 @@ class KNN(ModelTrainable):
         float
             An Accuracy score for knn
         """
-        #Normalize test dataset to fit knn
+        # Normalize test dataset to fit knn
         scaled_test_features, scaled_test_labels = self.normalized_dataset(self._test_ds)
-        
-        #Testing model and determining accuracy
+
+        # Testing model and determining accuracy
         prediction = self._train(self._best_hyperparams).predict(scaled_test_features)
         return None, metrics.accuracy_score(scaled_test_labels, prediction)
 
